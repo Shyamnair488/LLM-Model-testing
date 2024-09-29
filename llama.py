8bc98eaa24be9a575dbbeeb9558dc7f918f2f553
Shyam Nair <110102576+Shyamnair488@users.noreply.github.com>
2024-09-29 18:11:50 +0530

    first commit

---
+++ llama.py
@@ -0,0 +1,54 @@
+import subprocess
+
+
+def query_llama3(query):
+    try:
+        query = f"Answer in short (minimum 1 words): {query}"   # Change the word limit of your prompt according to your intrest
+        # Start the process
+        process = subprocess.Popen(
+            ["ollama", "run", "llama3"], 
+            stdin=subprocess.PIPE, 
+            stdout=subprocess.PIPE, 
+            stderr=subprocess.PIPE, 
+            text=True, 
+            encoding='utf-8'  # Explicitly set encoding to UTF-8
+        )
+        
+        # Send the query to the process
+        output, errors = process.communicate(input=query)
+        
+        # Print the output and errors (optional)
+        if output:
+            return output
+        
+        
+        else:
+            return errors
+        
+    
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred: {e}")
+        return None, str(e)
+    
+    
+if __name__ == '__main__':
+
+    while True:
+        print('What do you want to search')                
+        query = input("Enter your query: ")
+        if(query=="exit"):
+            break
+        else:
+            response = query_llama3(query)
+            print(response)
+
+
+
+
+
+
+# --------------------
+            
+
+
+
